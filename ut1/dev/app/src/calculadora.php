b4554ef9240815579f2c7041ba3d92eaf928f88c
AlejandroPerMar <aleperzmartin@gmail.com>
2023-10-08 22:39:18 +0100

    Se añade la aplicación calculadora dockerizada

---
+++ ut1/dev/app/src/calculadora.php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+    <title>Calculadora</title>
+</head>
+
+<body>
+    <h1>Calculadora</h1>
+    <form method="post" action="calculadora.php">
+        <input type="number" value=0 name="num1">
+        <br><br>
+        <input type="number" value=0 name="num2">
+        <br><br>
+        <select name="signo">
+            <option value="sumar">Sumar
+            </option>
+            <option value="restar">Restar
+            </option>
+            <option value="multiplicar">Multiplicar
+            </option>
+            <option value="dividir">Dividir
+            </option>   
+        </select>
+        <br><br>
+        <img src="/img/calculadora.png">
+        <br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
+
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$signo = $_POST['signo'];
+
+if ($signo == "sumar") {
+    $res = $num1 + $num2;
+} else if ($signo == "restar") {
+    $res = $num1 - $num2;
+} else if ($signo == "multiplicar") {
+    $res = $num1 * $num2;
+} else if ($signo == "dividir") {
+    $res = $num1 / $num2;
+}
+echo "La solución es: " . $res;
+?>
